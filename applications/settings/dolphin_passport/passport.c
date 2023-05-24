3274a0fe0dd7fec2301b5c8ce39fe1cfda986f47
jadenboss <134491835+jadenboss@users.noreply.github.com>
2023-05-24 06:25:39 -0700

    Update passport.c

--- applications/settings/dolphin_passport/passport.c
+++ applications/settings/dolphin_passport/passport.c
@@ -77,7 +77,7 @@ static void render_callback(Canvas* canvas, void* ctx) {
     canvas_draw_line(canvas, 58, 44, 123, 44);
 
     const char* my_name = furi_hal_version_get_name_ptr();
-    snprintf(level_str, 20, "test %hu", "...");
+    snprintf(level_str, 20, "No way 0-D %c", "ay");
     canvas_draw_str(canvas, 58, 12, my_name ? my_name : "Unknown");
     canvas_draw_str(canvas, 58, 26, mood_str);
     canvas_draw_str(canvas, 58, 40, level_str);
